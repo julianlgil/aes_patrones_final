c6af08605ca30cfeda483cc15ea4497656c005a5
SergioLeguizamon <sergioleguizamon_b@hotmail.com>
2024-05-26 11:42:40 -0500

    fix dockerfile context and avoid return null on get endpoint

--- crud_example/routers.py
+++ crud_example/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session, select
 
 from .database import get_session
@@ -17,9 +17,10 @@ def create_bill(bill: Bill, session: Session = Depends(get_session)):
 
 @router.get("/bills/{bill_id}")
 def read_bill(bill_id: int, session: Session = Depends(get_session)):
-    print("** ** ** bill_id 2 ** ** **", bill_id)
-
-    return session.get(Bill, bill_id)
+    bill = session.get(Bill, bill_id)
+    if not bill:
+        raise HTTPException(status_code=404, detail="Bill not found")
+    return bill
 
 
 @router.get("/bills/", response_model=list[Bill])
@@ -36,8 +37,10 @@ def read_all_bills(
 def update_bill(bill_id: int, bill: Bill, session: Session = Depends(get_session)):
     db_bill = session.get(Bill, bill_id)
     if not db_bill:
-        return {"error": "Bill not found"}
+        raise HTTPException(status_code=404, detail="Bill not found")
     for key, value in bill.dict().items():
+        if not value:
+            continue
         setattr(db_bill, key, value)
     session.commit()
     session.refresh(db_bill)
@@ -48,7 +51,7 @@ def update_bill(bill_id: int, bill: Bill, session: Session = Depends(get_session
 def delete_bill(bill_id: int, session: Session = Depends(get_session)):
     db_bill = session.get(Bill, bill_id)
     if not db_bill:
-        return {"error": "Bill not found"}
+        raise HTTPException(status_code=404, detail="Bill not found")
     session.delete(db_bill)
     session.commit()
     return {"message": "Bill deleted"}
