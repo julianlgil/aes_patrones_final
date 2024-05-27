e0afc81a2885d4928b6c0de003f3e89d7d83b52b
SergioLeguizamon <sergioleguizamon_b@hotmail.com>
2024-05-26 20:22:55 -0500

    remove dummy apps

--- crud_example/models.py
+++
@@ -1,6 +0,0 @@
-from sqlmodel import Field, SQLModel
-
-
-class Bill(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    amount: float
--- payments_dispatcher/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class DispatchRequest(BaseModel):
-    billId: int
-    action: str = Field(..., regex="^(query|pay)$")
