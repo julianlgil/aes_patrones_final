635c8fc6b5696ec98fff696d10c94cb4f6ec56c2
SergioLeguizamon <sergioleguizamon_b@hotmail.com>
2024-05-29 19:24:07 -0500

    add providers bills references

--- provider_mock/crud.py
+++ provider_mock/crud.py
@@ -1,11 +1,19 @@
-from typing import List, Optional
-from .schemas import Bill, BillCreate, BillUpdate
+from typing import Optional
+
+from .schemas import Bill, BillUpdate
 
 # Simulated in-memory database
 bills = [
     {"id": "12341", "amount": 100.0, "paid": False},
     {"id": "12342", "amount": 150.0, "paid": True},
     {"id": "12343", "amount": 200.0, "paid": False},
+    {"id": "12344", "amount": 300.0, "paid": False},
+    {"id": "12345", "amount": 400.0, "paid": False},
+    {"id": "12346", "amount": 500.0, "paid": False},
+    {"id": "12347", "amount": 600.0, "paid": False},
+    {"id": "12348", "amount": 700.0, "paid": False},
+    {"id": "12349", "amount": 800.0, "paid": False},
+    {"id": "123410", "amount": 900.0, "paid": False},
     {"id": "98761", "amount": 100.0, "paid": False},
     {"id": "98762", "amount": 150.0, "paid": True},
     {"id": "98763", "amount": 200.0, "paid": False},
